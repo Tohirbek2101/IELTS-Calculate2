736e5ff1dfaad7d1912d26e078bff3d9a7be1e37
Tohirbek <isomiddinovtohirjon5@gmail.com>
2026-01-09 10:17:50 -0700

    Add project files.

---
+++ IELTS-Calculate2/Process.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IELTS_calculate
+{
+    public class Process
+    {
+        public double Listening;
+        public double Reading;
+        public double Writing;
+        public double Speaking;
+        public double result;
+        public double CalculateOveral(double listening, double reading, double writing, double speaking)
+        {
+            this.Listening = listening;
+            this.Reading = reading;
+            this.Writing = writing;
+            this.Speaking = speaking;
+
+            double overall = (Listening + Reading + Writing + Speaking) / 4.0;
+            int result = (int)Math.Truncate(overall);
+            if (overall - result >= 0.25 && overall - result < 0.75)
+            {
+                return result + 0.5;
+            }
+            else if (overall - result >= 0.75)
+            {
+                return result + 1.0;
+            }
+            else
+            {
+                return result;
+            }
+
+
+        }
+
+    }
+}
+
